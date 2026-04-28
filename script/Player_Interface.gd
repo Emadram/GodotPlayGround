f5e926289f92a19dfd37439efa60b3ef9584cfff
Emadram <refemad@outlook.com>
2026-04-28 18:42:24 +0300

    Progress: Stable - Starting the unit selection - Basics DONE

---
+++ script/Player_Interface.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+# NODES
+@onready var player_camera
+@onready var player_camera_visibleunits_Area3D
+@onready var ui_select_rectangle
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
